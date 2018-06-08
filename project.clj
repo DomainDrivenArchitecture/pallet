d1b7697a6effaa2dc3c5155510d0315756656831
Jan Krebs <jan.krebs@meissa-gmbh.de>
2018-06-08 17:44:34 +0200

    Update stevedore dependency

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@ unprecedented level of customization."
                  [com.palletops/pallet-repl "0.8.0-beta.2"
                   :exclusions [com.palletops/pallet]]
                  ;do NOT upgrade script-exec to version 0.5.0 as it is older!
-                 [com.palletops/script-exec "0.4.2"]
+                 [com.palletops/script-exec "0.4.2" :exclusions [com.palletops/pallet-common]]
                  [dda/stevedore "0.8.1-SNAPSHOT"]
                  [clj-ssh "0.5.14"]
                  [enlive "1.1.6"
